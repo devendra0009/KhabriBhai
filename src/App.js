4ed3c50525af2e4b747a67ff637ea39a61c9f077
devendra0009 <devendrabedwal4@gmail.com>
2023-11-11 21:48:30 +0530

    routing issue sol2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import News from './components/News';
 import About from './components/About';
 import LoadingBar from 'react-top-loading-bar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import axios from 'axios';
 
@@ -28,7 +28,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Navbar categories={categories} />
         <LoadingBar height={3} color="#ff0000" progress={progress} />
         <Routes>
@@ -51,106 +51,8 @@ const App = () => {
                 />
               </>
             ))}
-          {/* <Route
-            path="/general"
-            element={
-              <News
-                setProgress={setProgress}
-                apiKey={apiKey}
-                key={'general'}
-                pageSize={pageSize}
-                country={'in'}
-                category={'general'}
-              />
-            }
-          />
-          <Route path="/about" element={<About category={'about'} />} />
-          <Route
-            exact
-            path="/business"
-            element={
-              <News
-                setProgress={setProgress}
-                apiKey={apiKey}
-                key={'business'}
-                pageSize={pageSize}
-                country={'in'}
-                category={'business'}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/entertainment"
-            element={
-              <News
-                setProgress={setProgress}
-                apiKey={apiKey}
-                key={'entertainment'}
-                pageSize={pageSize}
-                country={'in'}
-                category={'entertainment'}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/health"
-            element={
-              <News
-                setProgress={setProgress}
-                apiKey={apiKey}
-                key={'health'}
-                pageSize={pageSize}
-                country={'in'}
-                category={'health'}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/science"
-            element={
-              <News
-                setProgress={setProgress}
-                apiKey={apiKey}
-                key={'science'}
-                pageSize={pageSize}
-                country={'in'}
-                category={'science'}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/sports"
-            element={
-              <News
-                setProgress={setProgress}
-                apiKey={apiKey}
-                key={'sports'}
-                pageSize={pageSize}
-                country={'in'}
-                category={'sports'}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/technology"
-            element={
-              <News
-                setProgress={setProgress}
-                apiKey={apiKey}
-                key={'technology'}
-                pageSize={pageSize}
-                country={'in'}
-                category={'technology'}
-              />
-            }
-          /> */}
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
